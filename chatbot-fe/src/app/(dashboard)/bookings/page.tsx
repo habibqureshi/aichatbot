d95128f2ddfadb109db40945676c08c2a7052ce5
najeeb678 <najibqazi777@gmail.com>
2025-12-05 14:54:20 +0500

    updates

--- chatbot-fe/src/app/(dashboard)/bookings/page.tsx
+++ chatbot-fe/src/app/(dashboard)/bookings/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useInstallation } from "@/app/contexts/InstallationContext";
+// import { useInstallation } from "@/app/contexts/InstallationContext";
 import DoctorBookings from "@/components/dashboard/Bookings/DoctorsBookings";
-import ResturantBookings from "@/components/dashboard/Bookings/ResturantBookings";
+// import ResturantBookings from "@/components/dashboard/Bookings/ResturantBookings";
 
 export default function BookingsPage() {
-  const { installationType } = useInstallation();
-  return installationType === "clinic" ? <DoctorBookings /> : <ResturantBookings />;
+  // const { installationType } = useInstallation();
+  return <DoctorBookings />;
+  // return installationType === "clinic" ? <DoctorBookings /> : <ResturantBookings />;
 }

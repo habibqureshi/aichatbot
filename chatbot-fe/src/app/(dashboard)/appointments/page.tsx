b47ef2e505c317ed905953f7cd4efb61ba7aff16
najeeb678 <najibqazi777@gmail.com>
2025-12-10 16:09:15 +0500

    updated routes

---
+++ chatbot-fe/src/app/(dashboard)/appointments/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import DoctorBookings from "@/components/dashboard/Bookings/DoctorsBookings";
+export default function BookingsPage() {
+  return <DoctorBookings />;
+}
--- chatbot-fe/src/app/(dashboard)/doctors/manage/page.tsx
+++ chatbot-fe/src/app/(dashboard)/doctors/manage/page.tsx
@@ -465,32 +465,31 @@ function AddDoctorPageContent() {
 
           {/* Submit Section */}
           <div
-            className="backdrop-blur-sm border rounded-xl p-4 sm:p-6 shadow-sm"
+            className=" rounded-xl p-4 sm:p-6 shadow-sm"
             style={{
-              background: "#FFFFFF",
-              borderColor: "#F0EEFF",
+              background: "#F6F7F9",
             }}
           >
             <div className="space-y-4">
               {!isFormValid() && (
-                <div className="bg-amber-50 border border-amber-200 rounded-lg p-4">
-                  <div className="flex items-start gap-3">
+                <div className="bg-[#6325A912] rounded-lg p-3">
+                  <div className="flex items-center gap-2">
                     <svg
-                      className="w-5 h-5 text-amber-600 flex-shrink-0 mt-0.5"
+                      className="w-5 h-5 text-[#6325A9] flex-shrink-0 mt-0.5"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
                       <path
                         fillRule="evenodd"
-                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
                         clipRule="evenodd"
                       />
                     </svg>
                     <div className="flex-1">
-                      <p className="text-sm font-semibold text-gray-900 mb-2">
+                      <p className="text-xs font-semibold text-[#6325A9] mb-1">
                         Please complete all required fields:
                       </p>
-                      <ul className="text-sm space-y-1 ml-4 list-disc text-amber-800">
+                      <ul className="text-xs space-y-1 ml-4 list-disc text-[#6325A9]">
                         {formData.name.trim() === "" && <li>Doctor Name is required</li>}
                         {formData.phone_number.trim() === "" && <li>Phone Number is required</li>}
                         {formData.specialty.trim() === "" && <li>Specialty must be selected</li>}

03dac9e8f4719bdd820d3f905054549d5e5aa17b
hammadrafiqe2 <hammad.r@gltexpress.com>
2026-03-24 17:33:10 +0500

    custom voice

--- chatbot-be/services/appointment_service.py
+++ chatbot-be/services/appointment_service.py
@@ -1,6 +1,20 @@
 from logging import Logger
-from twilio.twiml.voice_response import VoiceResponse, Start, Gather
-from fastapi import HTTPException, BackgroundTasks
+from fastapi.websockets import WebSocketState
+from twilio.twiml.voice_response import Connect, VoiceResponse, Start, Gather
+from fastapi import (
+    HTTPException,
+    BackgroundTasks,
+    WebSocket,
+    WebSocketDisconnect,
+    WebSocketException,
+)
+import audioop
+import base64
+import asyncio
+import numpy as np
+import webrtcvad
+import whisper
+from piper import PiperVoice, SynthesisConfig
 from schemas.twilio import TwilioIncoming
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.datastructures import URL
@@ -13,13 +27,17 @@ from services import (
 from graph.bot_graph import get_graph
 from graph.intent_graph import voice_ai_graph
 from langgraph.graph.state import CompiledStateGraph, RunnableConfig
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from graph.appointmnet_graph import AppointmentState
 from twilio.rest import Client
 from configs import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 from langfuse import get_client
 from langfuse.langchain import CallbackHandler
 
+SAMPLING_RATE = 8000
+CHUNK_SIZE = 160
+VAD_AGGRESSIVENESS = 3
+SILENCE_THRESHOLD_MS = 500
 
 # Initialize Langfuse client
 langfuse = get_client()
@@ -31,7 +49,12 @@ langfuse_handler = CallbackHandler()
 
 background_tasks = BackgroundTasks()
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
+vad = webrtcvad.Vad(VAD_AGGRESSIVENESS)
+syn = SynthesisConfig(volume=0.7)
+whisper_model = whisper.load_model("small")
+voice = PiperVoice.load(
+    "/home/hammad/Documents/techbucks/custom-tts/en_US-lessac-medium.onnx"
+)
 VOICE = "Polly.Joanna-Neural"
 
 
@@ -307,3 +330,231 @@ async def test_intent(user_input: str, thread_id: str, log: Logger) -> str:
         intent = getattr(result, "next_agent", None)
     log.info(f"2 Intent test for '{user_input}' -> {intent}")
     return result["messages"]
+
+
+async def ws_greeting(
+    data: TwilioIncoming,
+    db: AsyncSession,
+    action_url: URL,
+    recording_status_callback: URL,
+    tenant_id: int,
+):
+    resp = VoiceResponse()
+    patient = await patient_service.find_or_create(
+        data.From, db=db, tenant_id=tenant_id
+    )
+    await conversation_service.find_or_create(
+        data=data, patient=patient, db=db, tenant_id=tenant_id
+    )
+    connect = Connect()
+    connect.stream(url=action_url)
+    resp.append(connect)
+    return resp
+
+
+async def stream_call(websocket: WebSocket, db: AsyncSession, tenant_id: int):
+    await websocket.accept()
+    stream_sid = None
+    audio_buffer = bytearray()
+    is_ai_responding = False
+    is_speaking = False
+    silence_counter = 0
+    messages: list[BaseMessage] = [
+        HumanMessage(content=f"[Caller phone: {patient.phone_number}]"),
+        HumanMessage(content=f"[Caller: {patient.name}]"),
+    ]
+    conversation = None
+    graph: CompiledStateGraph = None
+    patient = None
+    stop = False
+
+    async def handle_user_input(user_text: str, stream_sid: str):
+        nonlocal is_ai_responding, messages, graph, conversation, patient, db, tenant_id, stop
+        is_stop = False
+        try:
+            print(f"user input: {user_text}")
+            asyncio.create_task(
+                message_service.create(
+                    conversation=conversation,
+                    content=user_text,
+                    role="user",
+                    db=db,
+                    tenant_id=tenant_id,
+                )
+            )
+            ai_response = await graph.ainvoke(
+                AppointmentState(
+                    messages=messages + [HumanMessage(content=user_text)],
+                    user_input=user_text,
+                    patient_phone=patient.phone_number,
+                ),
+            )
+            final_message = ai_response.get("messages", [])[-1].content
+            print(f"AI response: {final_message}")
+            if not final_message:
+                final_message = "Due to some technical issues, we are unable to process your request at the moment. Our representative will get in touch with you shortly."
+                asyncio.create_task(
+                    conversation_service.needs_human(conversation=conversation, db=db)
+                )
+                is_stop = True
+            elif final_message.strip().endswith(
+                "FINISH_CONVERSATION"
+            ) or final_message.strip().endswith("**FINISH_CONVERSATION**"):
+                final_message = (
+                    final_message.replace("**FINISH_CONVERSATION**", "")
+                    .replace("FINISH_CONVERSATION", "")
+                    .strip()
+                )
+                if not final_message:
+                    final_message = "Thank you for contacting us. Goodbye!"
+                asyncio.create_task(
+                    conversation_service.end(conversation=conversation, db=db)
+                )
+                is_stop = True
+            elif final_message.strip().endswith(
+                "NEEDS_HUMAN_INTERVENTION"
+            ) or final_message.strip().endswith("**NEEDS_HUMAN_INTERVENTION**"):
+                final_message = (
+                    final_message.replace("**NEEDS_HUMAN_INTERVENTION**", "")
+                    .replace("NEEDS_HUMAN_INTERVENTION", "")
+                    .strip()
+                    or "Our representative will get in touch with you shortly."
+                )
+                asyncio.create_task(
+                    conversation_service.needs_human(conversation=conversation, db=db)
+                )
+                is_stop = True
+            for chunk in voice.synthesize(final_message, syn_config=syn):
+                resampled_chunk, _ = audioop.ratecv(
+                    chunk.audio_int16_bytes,
+                    2,  # Sample width (16-bit)
+                    1,  # Channels (Mono)
+                    22050,
+                    8000,
+                    None,
+                )
+                # 3. Convert Linear PCM to Mu-law (Twilio Format)
+                mu_law_chunk = audioop.lin2ulaw(resampled_chunk, 2)
+                for i in range(0, len(mu_law_chunk), CHUNK_SIZE):
+                    sub_chunk = mu_law_chunk[i : i + 160]
+                    if len(sub_chunk) < 160:
+                        sub_chunk = sub_chunk.ljust(160, b"\xff")
+
+                    payload = base64.b64encode(sub_chunk).decode("utf-8")
+                    if websocket.client_state != WebSocketState.CONNECTED:
+                        return
+                    await websocket.send_json(
+                        {
+                            "event": "media",
+                            "streamSid": stream_sid,
+                            "media": {"payload": payload},
+                        }
+                    )
+                    await asyncio.sleep(0.02)
+            messages.append(HumanMessage(content=user_text))
+            messages.append(AIMessage(content=final_message))
+            asyncio.create_task(
+                message_service.create(
+                    conversation=conversation,
+                    content=final_message,
+                    role="assistant",
+                    db=db,
+                    tenant_id=tenant_id,
+                )
+            )
+            is_ai_responding = False
+            stop = is_stop
+        except WebSocketException as e:
+            print(f"WebSocket error: {e}")
+        except WebSocketDisconnect:
+            print("Client disconnected")
+
+    try:
+        while True:
+            if stop:
+                break
+            data = await websocket.receive_json()
+            event = data.get("event")
+            match event:
+                case "connected":
+                    print(event)
+                    pass
+                case "start":
+                    stream_sid = data["start"]["streamSid"]
+                    print(f"Stream started: {stream_sid}")
+                    conversation = await conversation_service.find_by_call_sid(
+                        data["start"]["callSid"], db=db, tenant_id=tenant_id
+                    )
+                    if not conversation or conversation.status != "active":
+                        print("conversation already ended")
+                        break
+                    patient = await patient_service.find_by_id(
+                        conversation.patient_id, db, tenant_id=tenant_id
+                    )
+                    graph = await get_graph(
+                        data["start"]["callSid"],
+                        patient.phone_number,
+                        db=db,
+                        tenant_id=tenant_id,
+                    )
+                case "media":
+                    if is_ai_responding:
+                        continue
+                    payload = data["media"]["payload"]
+                    mu_law_chunk = base64.b64decode(payload)
+                    pcm_chunk = audioop.ulaw2lin(mu_law_chunk, 2)
+                    pcm_16k, _ = audioop.ratecv(
+                        pcm_chunk,
+                        2,
+                        1,
+                        8000,
+                        16000,
+                        None,
+                    )
+                    if vad.is_speech(pcm_chunk, 16000):
+                        if not is_speaking:
+                            print("Speech detected...")
+                            is_speaking = True
+                        silence_counter = 0
+                        audio_buffer.extend(pcm_16k)
+                    else:
+                        if is_speaking:
+                            silence_counter += 20  # Each chunk is 20ms
+                            audio_buffer.extend(pcm_16k)
+                        if is_speaking and silence_counter >= SILENCE_THRESHOLD_MS:
+                            is_ai_responding = True
+                            is_speaking = False
+                            print(
+                                f"Silence detected. Processing... {len(audio_buffer)}"
+                            )
+                            np_audio = (
+                                np.frombuffer(audio_buffer, dtype=np.int16).astype(
+                                    np.float32
+                                )
+                                / 32768.0
+                            )
+                            result = whisper_model.transcribe(
+                                np_audio, language="en", fp16=False
+                            )
+                            user_said = result.get("text", "")
+                            if user_said.strip():
+                                asyncio.create_task(
+                                    handle_user_input(user_said.strip(), stream_sid)
+                                )
+                            else:
+                                is_ai_responding = False
+                            audio_buffer.clear()
+                            is_speaking = False
+                            silence_counter = 0
+                case "stop":
+                    print("Stream stopped by client")
+                    await conversation_service.end(conversation=conversation, db=db)
+                    break
+        await websocket.close()
+    except WebSocketException as e:
+        print(f"WebSocket error: {e}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
+    finally:
+        # print(messages)
+        pass
